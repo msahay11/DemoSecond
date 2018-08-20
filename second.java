7795f2df0d4ff82e44869927d20f88435309398e
msahay11 <1406191@kiit.ac.in>
2018-08-20 16:00:55 +0530

    New changes

--- second.java
+++
@@ -1,6 +0,0 @@
-import java.util.*
-public static int main()
-{
-
-    System.out.ptrint("Hello");
-}
---
+++ src/abc/File2.java
@@ -0,0 +1,10 @@
+package abc;
+import java.util.*;
+
+public class File2
+{
+	public void method1()
+	{
+		System.out.println("Hello");
+	}
+}
